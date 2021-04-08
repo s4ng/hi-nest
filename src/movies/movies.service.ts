8104ea1cbbc74791ecd91cba415268259776dc49
s4ng <zalcls951201@gmail.com>
2021-04-08 15:44:39 +0900

    day first commit

---
+++ src/movies/movies.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id:string):Movie {
+    const movie = this.movies.find(movie => movie.id === +id);
+    if(!movie) {
+      throw new NotFoundException('Movie is not exist')
+    }
+    return movie;
+  }
+
+  delete(id:string) {
+    this.getOne(id)
+    this.movies = this.movies.filter(movie => movie.id !== +id);
+  }
+
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    })
+  }
+
+  update(id:string, updateData) {
+    const movie = this.getOne(id);
+    this.delete(id);
+    this.movies.push({...movie, ...updateData});
+  }
+}
